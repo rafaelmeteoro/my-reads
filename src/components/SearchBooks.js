0688332238732ff7681cf09202fc1a133dc54f15
Rafael Alves Feliciano <fael.anjelus@gmail.com>
2017-12-13 23:51:14 -0200

    feat: Search books from api
    Search books from api and showing in screen.

--- src/components/SearchBooks.js
+++ src/components/SearchBooks.js
@@ -1,22 +1,69 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import * as BooksAPI from '../utils/BooksAPI';
 
 class SearchBooks extends Component {
 
+    state = {
+        query: '',
+        books: []
+    }
+
+    updateQuery = (query) => {
+        this.setState({
+            query: query.trim()
+        })
+    }
+
+    searchBooks(query) {
+        BooksAPI.search(query).then(books => {
+            this.setState({ books })
+            console.log(books);
+        });
+    }
+
     render() {
+
+        const { books } = this.state;
+
         return (
             <div className="search-books">
                 <div className="search-books-bar">
-                    <Link className='close-search' to='/'>Close</Link>                    
+                    <Link className='close-search' to='/'>Close</Link>
                     <div className="search-books-input-wrapper">
                         {
                             //
                         }
-                        <input type="text" placeholder="Search by title or author" />
+                        <input
+                            type="text"
+                            placeholder="Search by title or author"
+                            onChange={(event) => this.searchBooks(event.target.value)}
+                        />
                     </div>
                 </div>
                 <div className="search-books-results">
-                    <ol className="books-grid"></ol>
+                    <ol className="books-grid">
+                        {books.map((book) => (
+                            <li key={book.id}>
+                                <div className="book">
+                                    <div className="book-top">
+                                        <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
+                                        <div className="book-shelf-changer">
+                                            <select>
+                                                <option value="none" disabled>Move to...</option>
+                                                <option value="currentlyReading">Currently Reading</option>
+                                                <option value="wantToRead">Want to Read</option>
+                                                <option value="read">Read</option>
+                                                <option value="none">None</option>
+                                            </select>
+                                        </div>
+                                    </div>
+                                    <div className="book-title">{book.title}</div>
+                                    <div className="book-authors">{book.authors}</div>
+                                </div>
+                            </li>
+                        ))}
+                    </ol>
                 </div>
             </div>
         )
