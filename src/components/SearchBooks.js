f0353e64063d4826b166f038b2da38fc96839d7d
Rafael Alves Feliciano <fael.anjelus@gmail.com>
2017-12-13 23:11:15 -0200

    feat: Add component router
    Insert component router and refactor to use Link to change page

--- src/components/SearchBooks.js
+++ src/components/SearchBooks.js
@@ -1,4 +1,5 @@
-import React, { Component } from React
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class SearchBooks extends Component {
 
@@ -6,7 +7,7 @@ class SearchBooks extends Component {
         return (
             <div className="search-books">
                 <div className="search-books-bar">
-                    <a className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</a>
+                    <Link className='close-search' to='/'>Close</Link>                    
                     <div className="search-books-input-wrapper">
                         {
                             //
