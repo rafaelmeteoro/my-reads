ed7eb7002eaa54612e4a0653ebbc18712dcdb941
Rafael Alves Feliciano <fael.anjelus@gmail.com>
2017-12-22 00:15:09 -0200

    refactor: Change LisBooks to functional component

--- src/components/ListBooks.js
+++ src/components/ListBooks.js
@@ -1,49 +1,40 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+import React from 'react';
 
-class ListBooks extends Component {
+const ListBooks = (props) => {
 
-    static propTypes = {
-        books: PropTypes.array.isRequired,
-        OnChangeBook: PropTypes.func.isRequired
-    }
+    const { books, OnChangeBook } = props;
 
-    render() {
-
-        const { books, OnChangeBook } = this.props;
-
-        return (
-            <div className='search-books-results'>
-                <ol className='books-grid'>
-                    {books.map((book) => (
-                        <li key={book.id}>
-                            <div className='book'>
-                                <div className='book-top'>
-                                    <div className='book-cover' style={{ width: 128, height: 192, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
-                                    <div className='book-shelf-changer'>
-                                        <select
-                                            onChange={e => {
-                                                OnChangeBook(book, e.target.value)
-                                            }}
-                                            value={book.shelf}
-                                        >
-                                            <option value='move' disabled>Move to...</option>
-                                            <option value='currentlyReading'>Currently Reading</option>
-                                            <option value='wantToRead'>Want to Read</option>
-                                            <option value='read'>Read</option>
-                                            <option value='none'>None</option>
-                                        </select>
-                                    </div>
+    return (
+        <div className='search-books-results'>
+            <ol className='books-grid'>
+                {books.map((book) => (
+                    <li key={book.id}>
+                        <div className='book'>
+                            <div className='book-top'>
+                                <div className='book-cover' style={{ width: 128, height: 192, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
+                                <div className='book-shelf-changer'>
+                                    <select
+                                        onChange={e => {
+                                            OnChangeBook(book, e.target.value)
+                                        }}
+                                        value={book.shelf}
+                                    >
+                                        <option value='move' disabled>Move to...</option>
+                                        <option value='currentlyReading'>Currently Reading</option>
+                                        <option value='wantToRead'>Want to Read</option>
+                                        <option value='read'>Read</option>
+                                        <option value='none'>None</option>
+                                    </select>
                                 </div>
-                                <div className='book-title'>{book.title}</div>
-                                <div className='book-authors'>{book.authors}</div>
                             </div>
-                        </li>
-                    ))}
-                </ol>
-            </div>
-        )
-    }
+                            <div className='book-title'>{book.title}</div>
+                            <div className='book-authors'>{book.authors}</div>
+                        </div>
+                    </li>
+                ))}
+            </ol>
+        </div>
+    );
 }
 
 export default ListBooks;
